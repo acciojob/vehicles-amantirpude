5e815dfa16c060daf123a11be7c833e7b26bd833
Aman <amantirpudetech@gmil.com>
2022-12-18 19:26:52 +0530

    Final Commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,7 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name,2,"XUV",4,5,isManual,5);
+        super(name,2,4,4,isManual,"XUV",5);
     }
 
     public void accelerate(int rate){

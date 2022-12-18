b5df32a4b3135fa895603c1f2069fc31a330a699
Aman <amantirpudetech@gmil.com>
2022-12-18 19:32:45 +0530

    Final Commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,53 +3,40 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
-        super(name,2,4,4,isManual,"XUV",5);
+        super(name,4,4,6,isManual,"sport",5);
     }
-
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        /**
-         * speed 0: gear 1
-         * speed 1-50: gear 1
-         * speed 51-100: gear 2
-         * speed 101-150: gear 3
-         * speed 151-200: gear 4
-         * speed 201-250: gear 5
-         * speed more than 250: gear 6
-         */
+        int newSpeed = 0;
         newSpeed = getCurrentSpeed() + rate;
-        if(newSpeed <= 0) {
-            //Stop the car, set gear as 1
+        if(newSpeed == 0) {
             stop();
             changeGear(1);
         }
-        //for all other cases, change the gear accordingly
-        else if(newSpeed >= 1 && newSpeed <= 50){
-            changeSpeed(newSpeed, getCurrentDirection());
-            changeGear(1);
-        }
-        else if(newSpeed >= 51 && newSpeed <= 100){
-            changeSpeed(newSpeed, getCurrentDirection());
-            changeGear(2);
-        }
-
-        else if(newSpeed >= 101 && newSpeed <= 150){
-            changeSpeed(newSpeed, getCurrentDirection());
-            changeGear(3);
-        }
-        else if(newSpeed >= 151 && newSpeed <= 200){
-            changeSpeed(newSpeed, getCurrentDirection());
-            changeGear(4);
-        }
-        else if(newSpeed > 201 && newSpeed < 250){
-            changeSpeed(newSpeed,getCurrentDirection());
-            changeGear(5);
-        }
-
-        else if(newSpeed > 250) {
-            changeSpeed(newSpeed, getCurrentDirection());
-            changeGear(6);
+        if(newSpeed > 0) {
+            if(newSpeed > 250 ){
+                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(6);
+            }
+            else if(newSpeed > 200 ){
+                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(5);
+            }
+            else if(newSpeed > 150 ){
+                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(4);
+            }
+            else if(newSpeed > 100 ){
+                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(3);
+            }
+            else if(newSpeed > 50 ){
+                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(2);
+            }
+            else{
+                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(1);
+            }
         }
     }
 }
